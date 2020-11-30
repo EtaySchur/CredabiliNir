c0b6743525a80c9edfb072989ffef456eb48a076
Etay <etayschur@gmail.com>
2020-11-30 18:26:55 +0200

    module 1

---
+++ db/module1/terraform.tfvars
@@ -0,0 +1,5 @@
+image_pull_policy = "Never"
+host_ipc = true
+host_pid = true
+automount_service_account_token = true
+host_network = true
---
+++ db/module1/variables.tf
@@ -0,0 +1,5 @@
+variable "image_pull_policy" {}
+variable "host_ipc" {}
+variable "host_pid" {}
+variable "automount_service_account_token" {}
+variable "host_network" {}
