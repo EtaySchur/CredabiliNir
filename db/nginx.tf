ef5128cd530df735202fb59cb803e93cbbebdafd
Etay <etayschur@gmail.com>
2020-12-08 18:06:11 +0200

    Add files via upload

---
+++ db/nginx.tf
@@ -0,0 +1,41 @@
+provider "kubernetes" {}
+
+resource "kubernetes_deployment" "risky" {
+  metadata {
+    name = "risky-ngnix-deployment-hc"
+		namespace = "default"
+    labels = {
+      app = "risky"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "risky"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "risky"
+        }
+      }
+
+      spec {
+				automount_service_account_token = true
+				host_ipc = true
+				host_pid = true
+				host_network = true
+        container {
+          image = "nginx"
+          name  = "risky-ngnix-container"
+					image_pull_policy = "Never"
+        }
+      }
+    }
+  }
+}
