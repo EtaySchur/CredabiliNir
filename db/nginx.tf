00380bf05de00e9c0bfcce663ed93db7f8d7f679
apolicy <apolicy@apolicy.io>
2020-12-09 13:59:31 +0000

    * Apolicy - remediate default:risky-ngnix-deployment-hc "SecurityContext.RunAsNonRoot" for control "Container permitting root"

--- db/nginx.tf
+++ db/nginx.tf
@@ -6,10 +6,10 @@ resource "kubernetes_deployment" "risky" {
     kubectl -n default patch deployment risky-redis-deployment-hc --patch "$(cat apolicy.risky-redis-deployment-hc.ImagePullPolicy.yaml)"
     EOH
   }
-  
+
   metadata {
-    name = "risky-ngnix-deployment-hc"
-		namespace = "default"
+    name      = "risky-ngnix-deployment-hc"
+    namespace = "default"
     labels = {
       app = "risky"
     }
@@ -32,17 +32,20 @@ resource "kubernetes_deployment" "risky" {
       }
 
       spec {
-				automount_service_account_token = true
-				host_ipc = true
-				host_pid = true
-				host_network = true
+        automount_service_account_token = true
+        host_ipc                        = true
+        host_pid                        = true
+        host_network                    = true
         container {
-          image = "nginx"
-          name  = "risky-ngnix-container"
-					image_pull_policy = "Never"
+          image             = "nginx"
+          name              = "risky-ngnix-container"
+          image_pull_policy = "Never"
 
           security_context {
             run_as_user = 1
+          }
+          securityContext {
+            run_as_non_root = true
           }
         }
       }
