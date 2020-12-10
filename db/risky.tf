548e009c344a5a11a9659e64ea8928acfa5fcd22
apolicy <apolicy@apolicy.io>
2020-12-10 07:14:44 +0000

    * Apolicy - remediate default:risky-redis-deployment-tf "ImagePullPolicy" for control "Container permanent image"

--- db/risky.tf
+++ db/risky.tf
@@ -62,7 +62,7 @@ resource "kubernetes_deployment" "risky_redis_deployment" {
             mount_path = "/mnt/risky"
           }
 
-          image_pull_policy = "Never"
+          image_pull_policy = "Always"
 
           security_context {
             capabilities {
@@ -83,4 +83,3 @@ resource "kubernetes_deployment" "risky_redis_deployment" {
     }
   }
 }
-
