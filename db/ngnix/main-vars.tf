795b0a7564b1a8038c1b4e09bef326936cafa84b
Etay <etayschur@gmail.com>
2020-12-13 17:44:22 +0200

    Add files via upload

---
+++ db/ngnix/main-vars.tf
@@ -0,0 +1,40 @@
+resource "kubernetes_deployment" "tf-variables-deployment" {
+  metadata {
+    name = "tf-variables-deployment"
+    namespace = "default"
+  }
+
+    spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "variables-tf-deployment"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "variables-tf-deployment"
+        }
+      }
+
+      spec {
+	automount_service_account_token = var.automount_service_account_token
+	host_ipc = var.host_ipc
+	host_pid = var.hostPid
+		
+        security_context {
+          run_as_non_root = var.run_as_non_root
+        }
+		
+        container {
+          image = "ngnix"
+          name  = "tfvars-ngnix-container"
+	  image_pull_policy = "Always"
+        }
+      }
+    }
+  }
+}
