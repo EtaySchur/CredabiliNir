5f11216faddd5b92e85809c31821d7f7bd2b95e9
Etay <etayschur@gmail.com>
2020-12-13 22:09:49 +0200

    Update and rename main-vars.tf to main.tf

--- db/ngnix/main.tf
+++ db/ngnix/main.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_deployment" "tf-variables-deployment" {
   metadata {
-    name = "tf-variables-deployment"
+    name      = "tf-variables-deployment"
     namespace = "default"
   }
 
@@ -22,14 +22,13 @@ resource "kubernetes_deployment" "tf-variables-deployment" {
 
       spec {
         automount_service_account_token = var.automount_service_account_token
-        host_ipc = var.host_ipc
-        host_pid = var.hostPid
+        host_ipc                        = var.host_ipc
+        host_pid                        = var.hostPid
 		
         container {
-          image = "ngnix"
-          name  = "tfvars-ngnix-container"
+          image             = "ngnix"
+          name              = "tfvars-ngnix-container"
 	  image_pull_policy = "Always"
-
           security_context {
              run_as_non_root = var.run_as_non_root
           }
