4e88822e0174755cfd164f9b138e67ef7cb92c9c
apolicy <apolicy@apolicy.io>
2020-12-13 11:06:30 +0000

    * Apolicy - remediate default:tf-variables-deployment "SecurityContext.RunAsUser" for control "Container running as root"

--- db/risky.tf
+++ db/risky.tf
@@ -76,7 +76,7 @@ resource "kubernetes_deployment" "risky_redis_deployment" {
         }
 
         container {
-          name = "some-second-container"
+          name  = "some-second-container"
           image = "mysql"
         }
 
@@ -92,11 +92,11 @@ resource "kubernetes_deployment" "risky_redis_deployment" {
 
 resource "kubernetes_deployment" "tf-variables-deployment" {
   metadata {
-    name = "tf-variables-deployment"
+    name      = "tf-variables-deployment"
     namespace = "default"
   }
 
-    spec {
+  spec {
     replicas = 1
 
     selector {
@@ -113,16 +113,19 @@ resource "kubernetes_deployment" "tf-variables-deployment" {
       }
 
       spec {
-				automount_service_account_token = var.automount_service_account_token
-				host_ipc = var.host_ipc
-				host_pid = var.hostPid
+        automount_service_account_token = var.automount_service_account_token
+        host_ipc                        = var.host_ipc
+        host_pid                        = var.hostPid
         security_context {
           run_as_non_root = var.run_as_non_root
         }
         container {
-          image = "ngnix"
-          name  = "tfvars-ngnix-container"
-					image_pull_policy = "Always"
+          image             = "ngnix"
+          name              = "tfvars-ngnix-container"
+          image_pull_policy = "Always"
+          security_context {
+            run_as_user = 56
+          }
         }
       }
     }
