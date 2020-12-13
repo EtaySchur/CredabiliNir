95fb5689b6dd615b82491208e060dc4914ae2168
apolicy <apolicy@apolicy.io>
2020-12-13 19:30:16 +0000

    * Apolicy - remediate default:tf-variables-deployment "HostIPC" for control "Workload sharing HostIPC"

--- db/ngnix/main-vars.tf
+++ db/ngnix/main-vars.tf
@@ -1,10 +1,10 @@
 resource "kubernetes_deployment" "tf-variables-deployment" {
   metadata {
-    name = "tf-variables-deployment"
+    name      = "tf-variables-deployment"
     namespace = "default"
   }
 
-    spec {
+  spec {
     replicas = 1
 
     selector {
@@ -22,16 +22,16 @@ resource "kubernetes_deployment" "tf-variables-deployment" {
 
       spec {
         automount_service_account_token = var.automount_service_account_token
-        host_ipc = var.host_ipc
-        host_pid = var.hostPid
-		
+        host_ipc                        = var.host_ipc
+        host_pid                        = var.hostPid
+
         container {
-          image = "ngnix"
-          name  = "tfvars-ngnix-container"
-	  image_pull_policy = "Always"
+          image             = "ngnix"
+          name              = "tfvars-ngnix-container"
+          image_pull_policy = "Always"
 
           security_context {
-             run_as_non_root = var.run_as_non_root
+            run_as_non_root = var.run_as_non_root
           }
         }
       }
