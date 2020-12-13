3bf2fab73f8caf626bb77c2eecef03af89edbd8b
Etay <etayschur@gmail.com>
2020-12-13 18:57:46 +0200

    Update main-vars.tf

--- db/ngnix/main-vars.tf
+++ db/ngnix/main-vars.tf
@@ -21,20 +21,21 @@ resource "kubernetes_deployment" "tf-variables-deployment" {
       }
 
       spec {
-	automount_service_account_token = var.automount_service_account_token
-	host_ipc = var.host_ipc
-	host_pid = var.hostPid
-		
-        security_context {
-          run_as_non_root = var.run_as_non_root
-        }
+        automount_service_account_token = var.automount_service_account_token
+        host_ipc = var.host_ipc
+        host_pid = var.hostPid
 		
         container {
           image = "ngnix"
           name  = "tfvars-ngnix-container"
 	  image_pull_policy = "Always"
+
+          security_context {
+             run_as_non_root = var.run_as_non_root
+          }
         }
       }
     }
   }
 }
+
