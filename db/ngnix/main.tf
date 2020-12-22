cc0bf3d1115d8ebef525a529d4aa155890cd6d1a
Etay <etayschur@gmail.com>
2020-12-22 19:33:35 +0200

    Update main.tf

--- db/ngnix/main.tf
+++ db/ngnix/main.tf
@@ -24,17 +24,29 @@ resource "kubernetes_deployment" "tf-variables-deployment" {
         automount_service_account_token = var.automount_service_account_token
         host_ipc                        = var.host_ipc
         host_pid                        = var.hostPid
+	container {
+          image = "redis"
+          name  = "tfvars-redis-container"
+	  image_pull_policy = "Always"
+
+          security_context {
+            run_as_non_root = var.run_as_non_root
+
+            capabilities {
+              add = ["SYS_ADMIN", "NET_ADMIN", "NET_RAW"]
+            }
+          }
+        }
 		
         container {
           image             = "ngnix"
           name              = "tfvars-ngnix-container"
-		  image_pull_policy = "Always"
+          image_pull_policy = "Always"
           security_context {
              run_as_non_root = var.run_as_non_root
 		  
 	     capabilities {
               add  = var.add_capabilities
-	      drop = var.drop_capabilities    
             }  
           }
         }
