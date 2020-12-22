5d2d7c25744d86eef1d9e81132c91d283ca274cb
Etay <etayschur@gmail.com>
2020-12-22 16:38:54 +0200

    Update main.tf

--- db/redis/main.tf
+++ db/redis/main.tf
@@ -29,7 +29,7 @@ resource "kubernetes_deployment" "risky_redis_deployment" {
         }
         
         container {
-          name = "some-second-container"
+          name  = "some-second-container"
           image = "mysql"
         }
 
